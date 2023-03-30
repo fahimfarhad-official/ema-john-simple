c618400739ef880e0f6bfea2abaf38e5e1cb504e
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-03-30 12:44:08 +0600

    cart item and her css add

--- src/Components/Header/Product/Product.jsx
+++ src/Components/Header/Product/Product.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import './product.css';
+import React from "react";
+import "./product.css";
 const Product = (props) => {
-    const {img, name, price, ratings, seller}= props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <h4>{name}</h4>
-            <h5>Price: ${price}</h5>
-            <h6>Manufacture: {seller}</h6>
-            <h6>Rating: {ratings} star</h6>
-            <button>Add to Cart</button>
-        </div>
-    );
+  const { img, name, price, ratings, seller } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <h5 className="price">Price: ${price}</h5>
+        <h6 className="h6">Manufacture: {seller}</h6>
+        <h5 className="h6">Rating: {ratings} star</h5>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
